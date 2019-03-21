e9040be52558d40b6d61bb83d72f1dc6661c27f6
SavaLione <monologuesplus@gmail.com>
2019-03-21 10:37:04 +0500

    UwU

---
+++ src/client/BattleshipRoyale.cpp
@@ -0,0 +1,6 @@
+#include "Registration.h"
+
+int main()
+{
+	return 0;
+}
---
+++ src/client/Registration.h
@@ -0,0 +1,25 @@
+#ifndef REGISTRATION_H
+#define REGISTRATION_H
+
+class Registration
+{
+private:
+    /* data */
+
+    void setFooter();
+public:
+    std::string getName();
+    Registration(/* args */);
+    ~Registration();
+};
+
+Registration::Registration(/* args */)
+{
+}
+
+Registration::~Registration()
+{
+}
+
+
+#endif // REGISTRATION_H
