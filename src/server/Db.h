cfb82cc5eba9375479a088ab71a145d4a4ec6563
SavaLione <monologuesplus@gmail.com>
2019-03-29 20:33:32 +0500

    Комменты к бд
    OwO
    (=^ ◡ ^=)

--- src/server/Db.h
+++ src/server/Db.h
@@ -26,20 +26,42 @@ private:
     char *messageError;
 
     /* func */
+
+    /* Открытие базы данных */
     void db_open();
+
+    /* Закытие базы данных */
     void db_close();
+
+    /* Создание базы данных */
     void db_create();
+
+    /* Проверка существования таблицы Player */
     bool db_table_check();
+
+    /* Вывод всех пользователей с информацией в консоль */
     void db_all_check();
-    void db_add_some();
+
+    /* Some */
+    //void db_add_some();
+
+    /* Вывод информации о пользователе поссылке */
     void db_get_player_print(db_player *pl);
+
+    /* Создание sha2 по ссылке */
     std::string sha2(std::string *s);
 public:
-
     /* func */
-    DB(/* args */);
+
+    DB();
+
+    /* Создать пользователя s_name с паролем s_password */
     void db_add_player(std::string s_name, std::string s_password);
+
+    /* Проверка существования пользователя s_name */
     bool db_check_player(std::string s_name);
+
+    /* Получить карточку пользователя db_player по ссылке */
     void db_get_player(std::string name, db_player *pl);
 
     /* des */
