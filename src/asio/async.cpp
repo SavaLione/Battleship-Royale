fe219a102e47d0c065d40b4a36d0991901b31584
SavaLione <monologuesplus@gmail.com>
2019-04-12 11:30:07 +0500

    no message

--- src/asio/async.cpp
+++ src/asio/async.cpp
@@ -1,6 +1,11 @@
 #include "Server.h"
 #include "con_handler.h"
 
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
 int main(int argc, char *argv[])
 {
 	try 
