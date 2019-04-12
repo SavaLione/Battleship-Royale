c1784bc14059935f3ca215fc76b9c7f4128cbcff
SavaLione <monologuesplus@gmail.com>
2019-04-12 13:04:06 +0500

    logger

--- src/asio/client/client.cpp
+++ src/asio/client/client.cpp
@@ -1,38 +1,53 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 using namespace boost::asio;
 using ip::tcp;
-using std::string;
 using std::cout;
 using std::endl;
+using std::string;
 
-int main() {
-  boost::asio::io_service io_service;
-  tcp::socket socket(io_service);
+int main()
+{
+    boost::asio::io_service io_service;
+    tcp::socket socket(io_service);
 
-  socket.connect( tcp::endpoint( boost::asio::ip::address::from_string("127.0.0.1"), 1234 ) );
+    socket.connect(tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 1234));
 
-  const string msg = "Hello from Client!\n";
-  boost::system::error_code error;
-  boost::asio::write( socket, boost::asio::buffer(msg), error );
+    const string msg = "[Message from Client]\n";
+    boost::system::error_code error;
+    boost::asio::write(socket, boost::asio::buffer(msg), error);
 
-  if( !error ) {
-    cout << "Client sent hello message!" << endl;
-  }
-  else {
-    cout << "send failed: " << error.message() << endl;
-  }
+    if (!error)
+    {
+        //cout << "Client sent hello message!" << endl;
+        spdlog::info("Client sent hello message!");
+    }
+    else
+    {
+        //cout << "send failed: " << error.message() << endl;
+        spdlog::error("Send failed: {}", error.message());
+    }
 
-  boost::asio::streambuf receive_buffer;
-  boost::asio::read(socket, receive_buffer, boost::asio::transfer_all(), error);
+    boost::asio::streambuf receive_buffer;
+    boost::asio::read(socket, receive_buffer, boost::asio::transfer_all(), error);
 
-  if( error && error != boost::asio::error::eof ) {
-    cout << "receive failed: " << error.message() << endl;
-  }
-  else {
-    const char* data = boost::asio::buffer_cast<const char*>(receive_buffer.data());
-    cout << data << endl;
-  }
-  return 0;
+    if (error && error != boost::asio::error::eof)
+    {
+        //cout << "receive failed: " << error.message() << endl;
+        spdlog::error("Receive failed: {}", error.message());
+    }
+    else
+    {
+        const char *data = boost::asio::buffer_cast<const char *>(receive_buffer.data());
+        //cout << "\n\nData from server:\n" << endl;
+        //cout << data << endl;
+        //cout << "End data from server\n" << endl;
+        spdlog::info("Data from server:");
+        spdlog::info(data);
+        spdlog::info("End data from server");
+    }
+    return 0;
 }
