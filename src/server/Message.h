df6d92955b9fae49edf53d5d44f82eb417214efa
Abs-Zer0 <33328791+Abs-Zer0@users.noreply.github.com>
2019-04-12 23:06:55 +0500

    можно примерно так делать сообщения. получится, что можно передавать любой объект, наследуемый от MessageObject

---
+++ src/server/Message.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string>
+
+class Message
+{
+private:
+	MessageObject object;
+
+protected:
+	virtual std::string code = "00|";
+
+public:
+	Message(MessageObject obj) : object(obj) { }
+
+	std::string ToSend()
+	{
+		return code + object.ToMessage();
+	}
+
+	MessageObject GetObject(const std::string & msg)
+	{
+		return MessageObject::FromMessage(std::string(msg, 3, msg.length - 3));
+	}
+};
+
+class MessageObject
+{
+public:
+	virtual std::string ToMessage()
+	{
+		return "";
+	}
+
+	static virtual MessageObject FromMessage(const std::string & msg)
+	{
+		return new MessageObject();
+	}
+};
