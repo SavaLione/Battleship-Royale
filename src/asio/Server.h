0205b835965c808f8692cf225969db8771e75890
SavaLione <monologuesplus@gmail.com>
2019-04-12 11:49:29 +0500

    no message

--- src/asio/Server.h
+++ src/asio/Server.h
@@ -2,7 +2,6 @@
 #define SERVER_H
 
 #include "con_handler.h"
-#include <iostream>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 #include <boost/enable_shared_from_this.hpp>
@@ -10,30 +9,14 @@
 class Server {
 private:
 	boost::asio::ip::tcp::acceptor acceptor_;
-
-	void start_accept()
-	{
-		// creates a socket
-		con_handler::pointer connection = con_handler::create(acceptor_.get_io_service());
-		// initiates an asynchronous accept operation 
-		// to wait for a new connection. 
-		acceptor_.async_accept(connection->socket(), boost::bind(&Server::handle_accept, this, connection, boost::asio::placeholders::error));
-	}
+	void start_accept();
 
 public:
-	Server(boost::asio::io_service& io_service): acceptor_(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 1234))
-	{ 
-		start_accept();
-	}
+	Server(boost::asio::io_service& io_service): acceptor_(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 1234));
+	void handle_accept(con_handler::pointer connection, const boost::system::error_code& err);
 
-	void handle_accept(con_handler::pointer connection, const boost::system::error_code& err)
-	{
-		if (!err)
-		{
-			connection->start();
-		}
-		start_accept();
-	}
+	/* des */
+	~Server();
 };
 
 #endif // SERVER_H
