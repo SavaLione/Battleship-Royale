6a0b09e4a52a3aa10018245a05058a6a11c3314c
SavaLione <monologuesplus@gmail.com>
2019-04-13 20:58:06 +0500

    regex

--- src/server/con_handler.cpp
+++ src/server/con_handler.cpp
@@ -1,9 +1,15 @@
 #include "con_handler.h"
 
+#include <string>
+
 #include <boost/bind.hpp>
+#include <boost/regex.hpp>
 
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
+
+#include "BattleshipRoyale.h"
+#include "DB.h"
 
 boost::asio::ip::tcp::socket& con_handler::socket()
 {
@@ -22,6 +28,18 @@ void con_handler::handle_read(const boost::system::error_code &err, size_t bytes
     {
         spdlog::info("--------------------------");
         spdlog::info(data);
+
+        boost::regex pattern_reg_user(BR::REG_USER);
+        //boost::regex pattern_reg_user_pass(BR::REG_USER_PASS);
+        //boost::regex pattern_reg_uid(BR::REG_UID);
+
+        boost::smatch result;
+
+        if(boost::regex_search(std::string(data), result, pattern_reg_user))
+        {
+
+        }
+
     }
     else
     {
