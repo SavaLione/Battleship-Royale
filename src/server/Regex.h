f7cd772b9370ef8e3d316ff5bd13913786f0d3c6
SavaLione <monologuesplus@gmail.com>
2019-04-13 22:04:33 +0500

    void getData(std::string *msg, std::string *pattern, std::string *ret_one); void getData(std::string *msg, std::string *pattern, std::string *ret_one, std::string *ret_two);

--- src/server/Regex.h
+++ src/server/Regex.h
@@ -4,5 +4,7 @@
 #include <string>
 
 bool check_pattern(std::string *msg, std::string *pattern);
+void getData(std::string *msg, std::string *pattern, std::string *ret_one);
+void getData(std::string *msg, std::string *pattern, std::string *ret_one, std::string *ret_two);
 
 #endif // REGEX_H
