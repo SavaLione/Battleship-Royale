18bb03bd86c8121557e5ea8aed290887e10cfe2f
SavaLione <monologuesplus@gmail.com>
2019-04-13 20:39:41 +0500

    comments

--- src/server/Server.h
+++ src/server/Server.h
@@ -9,14 +9,6 @@ private:
 	void start_accept();
 
 public:
-
-	/*
-	Server(boost::asio::io_service& io_service) : acceptor_(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 1234))
-	{ 
-		start_accept();
-	}
-	*/
-
 	Server(boost::asio::io_service& io_service, int i_port) : acceptor_(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), i_port))
 	{
 		start_accept();
