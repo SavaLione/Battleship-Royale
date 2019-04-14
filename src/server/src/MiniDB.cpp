7b9d69c9198924d557094eaa3d56db0757e37c40
SavaLione <monologuesplus@gmail.com>
2019-04-14 16:08:30 +0500

    OwO
    (=^ ◡ ^=)

---
+++ src/server/src/MiniDB.cpp
@@ -0,0 +1,60 @@
+#include "MiniDB.h"
+
+MiniDB::MiniDB()
+{
+    sqlite3_open(BR::DB_NAME, &db);
+
+    *sql = BR::SQLITE3_PRAGMA;
+    request(sql);
+}
+
+MiniDB::~MiniDB()
+{
+    sqlite3_close(db);
+    delete rc;
+    delete sql;
+}
+
+/**
+ * @brief Проверка существования пользователя
+ * @param [in] name имя пользователя
+ * @return true - пользователь существует, false - пользователь не существует.
+ */
+bool MiniDB::checkPlayer(std::string *name)
+{
+    *sql = "SELECT NAME FROM PLAYER WHERE NAME = \"";
+    *sql += *name;
+    *sql += "\";";
+
+    //const char *tail;
+
+    //*rc = sqlite3_prepare_v2(db, s_sql->c_str(), 1000, &stmt, &tail);
+    sqlite3_prepare_v2(db, sql->c_str(), 1000, &stmt, NULL);
+
+    while (sqlite3_step(stmt) == SQLITE_ROW)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+/**
+ * @brief Получение пароля по имени
+ * @param [in] name имя пользователя
+ * @param [out] пароль (из бд. в sha2)
+ */
+void MiniDB::getPassword(std::string *name, std::string *sha2_ret)
+{
+    //const char *tail;
+
+    *sql = "SELECT PASSWORD FROM PLAYER WHERE NAME =\"";
+    *sql += *name;
+    *sql += "\";";
+
+    sqlite3_prepare_v2(db, sql->c_str(), 1000, &stmt, NULL);
+    while (sqlite3_step(stmt) == SQLITE_ROW)
+    {
+        *sha2_ret = std::string(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0)));
+    }
+}
---
+++ src/server/src/MiniDB.h
@@ -0,0 +1,54 @@
+#ifndef MINIDB_H
+#define MINIDB_H
+
+#include <string>
+
+#include <sqlite3.h>
+
+#include "BattleshipRoyale.h"
+
+class MiniDB
+{
+private:
+    /**
+     * @brief Ответ базы данных
+     */
+    int *rc = new int;
+
+    /**
+     * @brief База данных
+     */
+    sqlite3 *db;
+
+    /**
+     * @brief Необходимо для получения данных из бд
+     */
+    sqlite3_stmt *stmt;
+
+    std::string *sql = new std::string;
+
+    inline void request(std::string *request)
+    {
+        sqlite3_exec(db, request->c_str(), NULL, 0, NULL);
+    }
+
+public:
+    MiniDB();
+    ~MiniDB();
+
+    /**
+     * @brief Проверка существования пользователя
+     * @param [in] name имя пользователя
+     * @return true - пользователь существует, false - пользователь не существует.
+     */
+    bool checkPlayer(std::string *name);
+
+    /**
+     * @brief Получение пароля по имени
+     * @param [in] name имя пользователя
+     * @param [out] пароль (из бд. в sha2)
+     */
+    void getPassword(std::string *name, std::string *sha2_ret);
+};
+
+#endif // MINIDB_H
