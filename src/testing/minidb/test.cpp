fdcd573ff3ba4d1ff80c5e7931168ed939ce0726
SavaLione <monologuesplus@gmail.com>
2019-04-15 12:56:24 +0500

    OK

--- src/testing/minidb/test.cpp
+++ src/testing/minidb/test.cpp
@@ -3,11 +3,27 @@
 
 #include "MiniDB.h"
 
+void createMoreObjects();
+
+const int OJECTS = 1000;
+
 int main()
 {
     MiniDB *mdb = new MiniDB;
     mdb->setTable();
     delete mdb;
     
+    ///
+    createMoreObjects();
+
     return 0;
+}
+
+void createMoreObjects()
+{
+    for(int i = 0; i < OJECTS; i++)
+    {
+        MiniDB *mdb = new MiniDB;
+        delete mdb;
+    }
 }
