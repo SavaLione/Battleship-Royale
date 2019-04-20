ecde2bd156b6cb27cf51a4c579293357a249daac
SavaLione <monologuesplus@gmail.com>
2019-04-21 00:21:16 +0500

    Test
    (=^ ◡ ^=)

--- src/client/src/BattleshipRoyale.cpp
+++ src/client/src/BattleshipRoyale.cpp
@@ -19,6 +19,8 @@
 #include "BattleshipRoyale.h"
 #include "Client.h"
 
+void test(int const& PORT, std::string const& s_ip);
+
 /**
  * @brief Точка входа в программу
  * @param [in] argc количество аргументов для парсера
@@ -80,13 +82,125 @@ int main(int argc, char *argv[])
     spdlog::info("IP: {}", s_ip);
     spdlog::info("PORT: {}", PORT);
 
-	Client ct;
-	ct.setPort(PORT);
-	ct.setIp(s_ip);
-	ct.connect();
-	ct.send(s_test);
-	spdlog::info(ct.receive());
+	// Client ct;
+	// ct.setPort(PORT);
+	// ct.setIp(s_ip);
+	// ct.connect();
+	// ct.send(s_test);
+	// spdlog::info(ct.receive());
+
+	test(PORT, s_ip);
 
     return 0;
+}
+
+/**
+ * |user_check true|user_check false|user pass true|user pass false|
+ * [ ][ ][ ][ ]
+ */
+void test(int const& PORT, std::string const& s_ip)
+{
+	spdlog::info("|user_check true|user_check false|user pass true|user pass false|");
+	spdlog::info("|---------------|----------------|--------------|---------------|");
+	int connections = 0;
+	while(true)
+	{
+		bool bt[] = { true, true , true, true};
+		connections++;
+		{
+			std::string msg = "user_check:[SavaLione]";
+			Client ct;
+			ct.setPort(PORT);
+			ct.setIp(s_ip);
+			ct.connect();
+			ct.send(msg);
+			if(ct.receive() != "answer:[true]")
+			{
+				bt[0] = false;
+			}
+		}
+
+		{
+			std::string msg = "user_check:[NOTNOTNOT]";
+			Client ct;
+			ct.setPort(PORT);
+			ct.setIp(s_ip);
+			ct.connect();
+			ct.send(msg);
+			if(ct.receive() != "answer:[false]")
+			{
+				bt[1] = false;
+			}
+		}
+
+		{
+			std::string msg = "user:[OwO] pass:[OwO]";
+			Client ct;
+			ct.setPort(PORT);
+			ct.setIp(s_ip);
+			ct.connect();
+			ct.send(msg);
+			if(ct.receive() != "UID:[OWWO111OwO123OwO111OwO]")
+			{
+				bt[2] = false;
+			}
+		}
+
+		{
+			std::string msg = "user:[Hewwwoo] pass:[Hiii]";
+			Client ct;
+			ct.setPort(PORT);
+			ct.setIp(s_ip);
+			ct.connect();
+			ct.send(msg);
+			if(ct.receive() == "answer:[true]")
+			{
+				bt[3] = false;
+			}
+		}
+
+		{
+			std::string s = "";
+			if(bt[0])
+			{
+				s += "[ ]";
+			}
+			else
+			{
+				s += "[X]";
+			}
+
+			if(bt[1])
+			{
+				s += "[ ]";
+			}
+			else
+			{
+				s += "[X]";
+			}
+
+			if(bt[2])
+			{
+				s += "[ ]";
+			}
+			else
+			{
+				s += "[X]";
+			}
+
+			if(bt[3])
+			{
+				s += "[ ]";
+			}
+			else
+			{
+				s += "[X]";
+			}
+			s += " ";
+			s += connections;
+			
+			spdlog::info(s);
+		}
+	}
 }
 /** @} */
